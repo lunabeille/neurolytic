bdc8d47498f1134f05ec4fba1a9254f2b00e5c07
lunabeille <lucy.bouquet@gmail.com>
2017-06-10 14:39:19 +0200

    Added : employe and service class base strucutre

---
+++ src/swing/Employee.java
@@ -0,0 +1,36 @@
+package swing;
+
+public class Employee 
+{
+	String name;
+	String firstname;
+	char sexe;
+	String birthdate;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public char getSexe() {
+		return sexe;
+	}
+	public void setSexe(char sexe) {
+		this.sexe = sexe;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	
+}
---
+++ src/swing/Service.java
@@ -0,0 +1,20 @@
+package swing;
+
+public class Service 
+{
+	String name;
+	Employee [] team;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Employee[] getTeam() {
+		return team;
+	}
+	public void setTeam(Employee[] team) {
+		this.team = team;
+	}
+}
